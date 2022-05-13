14218a0271227ee0580509f00f732db5115d9e64
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2022-05-13 18:40:39 +0200

    update webscrapping

--- SoSe_2022/Webscraping/examples/functions.R
+++ SoSe_2022/Webscraping/examples/functions.R
@@ -1,23 +1,54 @@
-########## functions 
+########## functions
+
+
+extract_links <- function(url){
+  
+  tibble(url = read_html(url) %>%
+           html_elements('.noProject-eaed4') %>%
+           html_attr('href')
+  )
+}
+
+get_links<- function(locations, num_pages){
+  i <- 1
+  repeat{
+    if(i == 1){
+      df <- extract_links(paste0('https://www.immowelt.de/liste/' , locations,
+                                 '/wohnungen/mieten?d=true&sd=DESC&sf=RELEVANCE&sp=', i))
+      print(paste(locations, i))
+    } else {
+      temp_df <- extract_links(paste0('https://www.immowelt.de/liste/' , locations,
+                                      '/wohnungen/mieten?d=true&sd=DESC&sf=RELEVANCE&sp=', i))
+      if(any(duplicated(rbind(df, temp_df)))) break
+      df <- rbind(df, temp_df)
+      print(paste(locations, i))
+      if(i == num_pages) break
+    }
+    i <- i + 1
+    Sys.sleep(sample(2:10, 1))
+  }
+  return(df)
+}
+
 # title 
 get_title <- function(html){
   html |>
-    html_nodes('h1') |> 
-    html_text() |> 
+    html_nodes('h1') %>% 
+    html_text() %>%
     str_trim()
 }
 
 ### locations (zipcode)
 get_location <- function(html){
-  df <- html |>
-    html_nodes('#exposeAddress div') |> 
-    html_text() |> 
-    str_trim() |>
-    str_split(' ') |> 
-    unlist() |>
-    str_replace_all("\\(|\\)", "") |>
-    matrix(nrow = 1) |>
-    as_tibble() |>
+  df <- html %>%
+    html_nodes('#exposeAddress div') %>%
+    html_text() %>%
+    str_trim() %>%
+    str_split(' ') %>%
+    unlist() %>%
+    str_replace_all("\\(|\\)", "") %>%
+    matrix(nrow = 1) %>%
+    as_tibble() %>%
     dplyr::rename('zipcode' = 1, 
                   'city' = 2,
                   'district' = 3
@@ -26,36 +57,113 @@ get_location <- function(html){
 }
 
 
-# Warmmiete
-get_warm.rent <- function(html){
+# alle mieten
+get_rents <- function(html){
   
-  df <- html %>%
+  tibble(class = c('Kaltmiete', 'Nebenkosten', 'Heizkosten in Warmmiete enthalten', 
+                   'Heizkosten nicht in Warmmiete enthalten', 'Warmmiete')) %>%
+    left_join(html %>%
+                html_nodes('#aPreise .cell__row') %>% 
+                html_text() %>%
+                str_trim() %>%
+                as_tibble() %>%
+                separate(value, into = c('class', 'value'), sep = "(?<=[a-zA-Z])\\s*(?=[0-9])")
+              , by = 'class'
+    ) %>%
+    dplyr::mutate(value = readr::parse_number(value, 
+                                              locale = locale(decimal_mark = ",", 
+                                                              grouping_mark = "."))) %>%
+    pivot_wider(names_from = class, values_from = value) %>%
+    dplyr::rename(
+      'cold_rent' = 'Kaltmiete',
+      'service_charges' = 'Nebenkosten',
+      'heating_cost_included' = 'Heizkosten in Warmmiete enthalten',
+      'heating_cost_excluded' = 'Heizkosten nicht in Warmmiete enthalten',
+      'warm_rent' = 'Warmmiete') # Heizkosten in oder nicht in Warmmiete
+}
+
+get_cold.rent <- function(html){
+  suppressWarnings(
+  html %>%  
     html_nodes('#aPreise .cell__row') %>% 
     html_text() %>%
     str_trim() %>%
     as_tibble() %>%
-    dplyr::filter(stringr::str_detect(value, 'Warmmiete') & !stringr::str_detect(value, 'Heizkosten')) %>%
-    pull() 
-  
-  ifelse(length(df) == 0, NA, 
-         readr::parse_number(df, locale = locale(decimal_mark = ",", grouping_mark = ".")))
-  
+    separate(value, into = c('class', 'value'), sep = "(?<=[a-zA-Z])\\s*(?=[0-9])") %>%
+    dplyr::filter(str_detect(class, 'Kaltmiete')) %>%
+    dplyr::mutate(value = readr::parse_number(value, 
+                                            locale = locale(decimal_mark = ",", 
+                                                            grouping_mark = "."))) %>%
+    dplyr::pull(value)) %>%
+    ifelse(is_empty(.), NA, .)
 }
 
-
-get_cold.rent <- function(html){
-  
-  df <- html %>%
-    html_nodes('#aPreise .cell__row') %>%  #.cell__row:nth-child(4)
-    html_text() %>%
-    str_trim() %>%
-    as_tibble() %>%
-    dplyr::filter(stringr::str_detect(value, 'Kaltmiete')) %>%
-    pull() 
-  
-  ifelse(length(df) == 0, NA, 
-         readr::parse_number(df, locale = locale(decimal_mark = ",", grouping_mark = ".")))
+get_warm.rent <- function(html){
+  suppressWarnings(
+    html %>%  
+      html_nodes('#aPreise .cell__row') %>% 
+      html_text() %>%
+      str_trim() %>%
+      as_tibble() %>%
+      separate(value, into = c('class', 'value'), sep = "(?<=[a-zA-Z])\\s*(?=[0-9])") %>%
+      dplyr::filter(str_detect(class, 'Warmmiete') & !str_detect(class, 'Heizkosten')) %>%
+      dplyr::mutate(value = readr::parse_number(value, 
+                                                locale = locale(decimal_mark = ",", 
+                                                                grouping_mark = "."))) %>%
+      dplyr::pull(value)) %>%
+    ifelse(is_empty(.), NA, .)
 }
+
+get_service.charges <- function(html){
+  suppressWarnings(
+    html %>%  
+      html_nodes('#aPreise .cell__row') %>% 
+      html_text() %>%
+      str_trim() %>%
+      as_tibble() %>%
+      separate(value, into = c('class', 'value'), sep = "(?<=[a-zA-Z])\\s*(?=[0-9])") %>%
+      dplyr::filter(str_detect(class, 'Nebenkosten')) %>%
+      dplyr::mutate(value = readr::parse_number(value, 
+                                                locale = locale(decimal_mark = ",", 
+                                                                grouping_mark = "."))) %>%
+      dplyr::pull(value)) %>%
+    ifelse(is_empty(.), NA, .)
+}
+
+get_heating.cost.included <- function(html){
+  suppressWarnings(
+    html %>%  
+      html_nodes('#aPreise .cell__row') %>% 
+      html_text() %>%
+      str_trim() %>%
+      as_tibble() %>%
+      separate(value, into = c('class', 'value'), sep = "(?<=[a-zA-Z])\\s*(?=[0-9])") %>%
+      dplyr::filter(str_detect(class, 'Heizkosten in Warmmiete enthalten')) %>%
+      dplyr::mutate(value = readr::parse_number(value, 
+                                                locale = locale(decimal_mark = ",", 
+                                                                grouping_mark = "."))) %>%
+      dplyr::pull(value)) %>%
+    ifelse(is_empty(.), NA, .)
+}
+
+get_heating.cost.excluded <- function(html){
+  suppressWarnings(
+    
+    html %>%  
+      html_nodes('#aPreise .cell__row') %>% 
+      html_text() %>%
+      str_trim() %>%
+      as_tibble() %>%
+      separate(value, into = c('class', 'value'), sep = "(?<=[a-zA-Z])\\s*(?=[0-9])") %>%
+      dplyr::filter(str_detect(class, 'Heizkosten nicht in Warmmiete enthalten')) %>%
+      dplyr::mutate(value = readr::parse_number(value, 
+                                                locale = locale(decimal_mark = ",", 
+                                                                grouping_mark = "."))) %>%
+      dplyr::pull(value) %>%
+      ifelse(is_empty(.), NA, .)
+    )
+}
+
 
 # rent, square meter, rooms
 get_size <- function(html){
@@ -72,33 +180,6 @@ get_size <- function(html){
     )
 }
 
-# get energy class
-get_energy.class <- function(html){
-  html %>%
-    html_nodes('.ng-star-inserted:nth-child(5) p') %>% 
-    html_text() %>% 
-    str_trim() %>%
-    .[2]
-}
-
-# energy consumption
-get_energy.cons <- function(html){
-  html %>%
-    html_nodes('.ng-star-inserted:nth-child(6) .has-font-75+ p') %>% 
-    html_text() %>% 
-    str_trim() %>%
-    readr::parse_number(locale = locale(decimal_mark = ",", grouping_mark = "."))
-}
-
-# build year
-get_year <- function(html){
-  html %>%
-    html_nodes('app-energy .cell:nth-child(3) p') %>% 
-    html_text() %>% 
-    str_trim() %>%
-    .[2]
-}
-
 # deposit
 get_deposit <- function(html){
   html %>%
@@ -108,13 +189,61 @@ get_deposit <- function(html){
     readr::parse_number(locale = locale(decimal_mark = ",", grouping_mark = "."))
 }
 
-# Building type
-get_building.type <- function(html){
-  html %>%
-    html_nodes('.energy_information .ng-star-inserted:nth-child(2) .has-font-75+ p') %>% 
-    html_text() %>% 
-    str_trim()
+get_energy.information <- function(html){
+ tibble(
+    class = c('Gebäudetyp', 'Baujahr laut Energieausweis',
+              'Effizienzklasse', 'Endenergiebedarf', 'Endenergieverbrauch')) %>%
+    left_join(
+      tibble(
+      class = html %>%
+        html_nodes('.energy_information .has-font-75') %>%
+        html_text() %>%
+        str_trim(),
+      
+      value = html %>%
+        html_nodes('.energy_information .has-font-75+ p') %>% 
+        html_text() %>% 
+        str_trim()
+      ),
+      by = 'class') %>%
+    pivot_wider(names_from = class, values_from = value) %>%
+    dplyr::mutate(energy_demand = case_when(
+      is.na(Endenergiebedarf) & !is.na(Endenergieverbrauch) ~ Endenergieverbrauch,
+      !is.na(Endenergiebedarf) &  is.na(Endenergieverbrauch) ~ Endenergiebedarf,
+      TRUE ~ NA_character_ 
+      )) %>%
+    dplyr::select(-c(Endenergiebedarf, Endenergieverbrauch)) %>%
+    dplyr::mutate(energy_demand = readr::parse_number(energy_demand,
+                                                         locale = locale(decimal_mark = ",", 
+                                                                         grouping_mark = "."))) %>%
+    dplyr::rename('building_type' = Gebäudetyp,
+                  'building_year' = `Baujahr laut Energieausweis`, 
+                  'efficiency_class' = Effizienzklasse) 
+  
 }
 
+
+
+
+# extracting data per side 
+extract_page_data <- function(url, row_num, ...){ #  row_num,
+  html <- read_html(url) 
+  df <-  tibble(title = get_title(html),
+                get_location(html),
+                cold_rent = get_cold.rent(html),
+                heating_cost_excluded = get_heating.cost.excluded(html),
+                heating_cost_included = get_heating.cost.included(html),
+                service_charges = get_service.charges(html),
+                warm_rent = get_warm.rent(html),
+                deposite = get_deposit(html), 
+                get_size(html) ,
+                get_energy.information(html),
+                url = url
+  )
+  print(paste(row_num, "of", nrow(data)))
+  
+  Sys.sleep(sample(2:7, 1))
+  return(df)
+}
 
 
--- SoSe_2022/Webscraping/examples/immowelt.R
+++ SoSe_2022/Webscraping/examples/immowelt.R
@@ -10,173 +10,19 @@ source(here::here('SoSe_2022/Webscraping/examples/functions.R'))
 locations <- c('essen', 'bochum')
 
 
-url <- 'https://www.immowelt.de/liste/essen/wohnungen/mieten?sort=relevanz'
-url <- 'https://www.immowelt.de/liste/essen/wohnungen/mieten?d=true&sd=DESC&sf=RELEVANCE&sp=1'
 
 #---- getting links ----
 
 # getting links 
 
-extract_links <- function(url){
-  
-  tibble(url = read_html(url) %>%
-           html_elements('.noProject-eaed4') %>%
-           html_attr('href')
-  )
-}
-
-get_links<- function(locations, num_pages){
-  i <- 1
-  repeat{
-    if(i == 1){
-      df <- extract_links(paste0('https://www.immowelt.de/liste/' , locations,
-                                 '/wohnungen/mieten?d=true&sd=DESC&sf=RELEVANCE&sp=', i))
-      print(paste(locations, i))
-    } else {
-      temp_df <- extract_links(paste0('https://www.immowelt.de/liste/' , locations,
-                                      '/wohnungen/mieten?d=true&sd=DESC&sf=RELEVANCE&sp=', i))
-      if(any(duplicated(rbind(df, temp_df)))) break
-      df <- rbind(df, temp_df)
-      print(paste(locations, i))
-      if(i == num_pages) break
-    }
-    i <- i + 1
-    Sys.sleep(sample(2:10, 1))
-  }
-  return(df)
-}
-
-
-
 # getting dataframe 
-data <- map2_dfr(locations, c(2, 2), get_links) #, .id = "company") 
+data <- map2_dfr(locations, c(5, 5), get_links)
 
-html <- read_html(data[[1,1]])
-
-extract_page_data <- function(url, ...){
-  html <- read_html(url) 
- df <-  tibble(title = get_title(html),
-         get_location(html),
-         warm_rent = get_warm.rent(html),
-         cold_rent = get_cold.rent(html),
-         deposite = get_deposit(html), 
-         get_size(html) ,
-         building_type = get_building.type(html),
-         energy_class = get_energy.class(html),
-         energy_consumption = get_energy.cons(html),
-         build_year = get_year(html)
-  )
- 
- return(df)
- 
- Sys.sleep(sample(2:15, 1))
-}
-
-data_try <- data %>%
-  slice_sample(n = 3)
-
-data_try %<>%
+# scraping data 
+data %<>%
+  dplyr::mutate(row_num = rownames(.)) %>%
   purrr::pmap_dfr(extract_page_data)
-  
-url <- data_try[[3,1]]
-
-extract_page_data(data_try[[1,1]])
 
 
-get_location(html)
-
-# prices
-
-
-html <- read_html(data_try[[2,1]])
-
-
-get_warm.rent(html)
-get_prices(html)
-get_size(html)
-get_energy.class(html)
-get_year(html)
-get_deposit(html)
-get_energy.cons(html)
-get_building.type(html)
-
-
-#####################
-######## try ########
-#####################
-# all prices 
-html %>%
-  html_nodes('#aPreise .card') %>% 
-  html_text() %>% 
-  str_trim() 
-  
-
-html_sub %>%
-  html_nodes('#aPreise .card') %>% 
-  html_text() %>% 
-  str_trim()
-  
-  
-
-
-html %>%
-  html_nodes('.hardfact__label') %>% 
-  html_text() %>% 
-  str_trim() %>%
-  readr::parse_number()
-
-# rent, square meter, rooms
-html %>%
-  html_nodes('.has-font-300') %>% 
-  html_text() %>% 
-  str_trim() %>%
-  stringr::str_replace_all(',', '.') %>%
-  readr::parse_number()
-
-# Energy 
-
-# energy class
-html %>%
-  html_nodes('.ng-star-inserted:nth-child(5) p') %>% 
-  html_text() %>% 
-  str_trim() 
-
-
-# energy consumption
-html %>%
-  html_nodes('.ng-star-inserted:nth-child(6) .has-font-75+ p') %>% 
-  html_text() %>% 
-  str_trim() 
-
-
-# Building type
-html %>%
-  html_nodes('.energy_information .ng-star-inserted:nth-child(2) .has-font-75+ p') %>% 
-  html_text() %>% 
-  str_trim() 
-
-
-
-
-
-# ausstattung
-html %>%
-  html_nodes('app-details .ng-star-inserted') %>% 
-  html_text() %>% 
-  str_trim() 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# parsing der Orte Verbessern :-)   
 
