7911a35a5f22236da4a7ca4c20e9f8413ba1065c
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2022-05-19 16:04:59 +0200

    assignment update
    - git
    - data
    - databses

--- SoSe_2022/Webscraping/examples/immowelt_analysis.R
+++ SoSe_2022/Webscraping/examples/immowelt_analysis.R
@@ -152,3 +152,110 @@ DBI::dbListTables(con)
 DBI::dbDisconnect(con)
 
 rm(con)
+
+
+library(dplyr)
+con_2 <- DBI::dbConnect(
+  drv = RSQLite::SQLite(),
+  dbname = "C:/Users/jens.klenke/Downloads/assignment_1.sqlite3", 
+)
+
+DBI::dbListTables(con_2)
+
+
+appointments_db <- dplyr::tbl(con_2, 'appointments')
+
+show_query(appointments_db)
+
+table <- collect(appointments_db, n = Inf)
+ 
+
+appointments_1 <- dplyr::tbl(con, 'appointments')
+
+table_1 <- collect(appointments_1, n = Inf)
+
+identical(table_1, table)
+
+
+
+#### try new data ####
+
+credit_card <- readxl::read_xls(here::here('SoSe_2022/datasets/default_of_credit_card_clients.xls'))
+
+names(credit_card)
+
+
+credit_card %>%
+  distinct(PAY_0)
+
+library(ggplot2)
+library(lubridate)
+#### tram ####
+
+metro <- readr::read_csv(here::here('SoSe_2022/datasets/Metro_Interstate_Traffic_Volume.csv'))
+
+names(metro)
+
+str(metro)
+class(metro$date_time)
+
+
+### database  
+  
+library(dplyr)
+  con <- DBI::dbConnect(
+    drv = RSQLite::SQLite(),
+    dbname = here::here('SoSe_2022/databases/Metro_Interstate_Traffic_Volume.sqlite3'), 
+  )  
+
+  DBI::dbWriteTable(con,
+                    name = "metro", 
+                    value = metro)
+  str(metro)
+  str(metro_db)
+# table connection 
+  
+metro_db <- tbl(con, 'metro')
+  
+# collect data 
+
+metro_db <- tbl(con, 'metro') %>%
+  collect(n = Inf)
+
+
+str(metro_db)
+
+metro_db %>%
+  distinct(weather_main)      
+
+## compute weekdays
+
+#functions
+# own function for theme 
+# function for theme 
+
+day_levels = c('Monday', 'Tuesday', 'Wednesday', 
+               'Thursday', 'Friday', 'Saturday', 'Sunday')   
+
+try <- metro_db %>%
+  dplyr::mutate(date_time = as_datetime(date_time),
+                weekday = factor(weekdays(date_time), 
+                                 levels = day_levels))
+
+try %>%
+ggplot2::ggplot(aes(x = weekday, y = traffic_volume)) +
+  ggplot2::geom_boxplot()
+
+
+# test
+
+# lineplot 
+metro %>%
+  dplyr::filter(date_time >= lubridate::ymd_hms('2014-01-01 00:00:00') & date_time <= lubridate::ymd_hms('2015-12-31 24:00:00')) %>%
+  ggplot(aes(x = date_time, y = traffic_volume)) +
+  geom_line(size = 0.000000000001, alpha = 0.2)
+
+# Jahres vergleich
+
+
+
