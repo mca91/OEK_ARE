a36fc3f7b38aaa6be38beee6878792422d5c26a2
mca91 <martin.emwikts@googlemail.com>
2025-05-09 12:24:52 +0200

    updates in OOP

--- SoSe_2025/Advanced R Concepts/bench_com_inferno.R
+++ SoSe_2025/Advanced R Concepts/bench_com_inferno.R
@@ -1,8 +1,9 @@
-bench::mark(df = {
 x <- data.frame(
   matrix(runif(5 * 1e6), 
          ncol = 100)
 )
+
+bench::mark(df = {
 medians <- apply(X = x, MARGIN =  2, FUN = median)
 #tracemem(x)
 for (i in seq_along(medians)) {
@@ -10,13 +11,10 @@ for (i in seq_along(medians)) {
 }},
 
 list = {
-x <- data.frame(
-  matrix(runif(5 * 1e6), 
-         ncol = 100)
-)
+medians <- apply(X = x, MARGIN =  2, FUN = median)
 y <- as.list(x)
 #tracemem(y)
-for (i in 1:5) {
+for (i in 1:100) {
   y[[i]] <- y[[i]] - medians[[i]]
 }
 }
