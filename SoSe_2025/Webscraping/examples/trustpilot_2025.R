091750a1d54e90f5ccc98e33c3cbc93050971281
Martin Schmelzer <schmelzer.martin@gmx.de>
2025-05-23 09:22:10 +0200

    Adding rmarkdown content. Make webscraping ex solution uploadable.

--- SoSe_2025/Webscraping/examples/trustpilot_2025.R
+++ SoSe_2025/Webscraping/examples/trustpilot_2025.R
@@ -3,7 +3,6 @@ library(rvest)
 library(lubridate)
 
 # Utility functions -------------------------------------------------------
-
 get_trimmed_text <- function(html, selector) {
   html %>% html_elements(selector) %>% html_text(trim = TRUE)
 }
@@ -25,7 +24,7 @@ extract_page_data <- function(company, page_url) {
   tibble(
     company = company,
     author  = get_trimmed_text(cards, ".styles_consumerDetails__POC79 > span"),
-    date    = get_html_attr(cards, "time", "datetime") %>% ymd_hms(),
+    date    = get_html_attr(cards, ".styles_reviewCardInnerHeader__8Xqy8 time", "datetime") %>% ymd_hms(),
     rating  = get_html_attr(cards, ".star-rating_starRating__sdbkn > img", "alt") %>%
       str_extract("\\d") %>% as.integer(),
     title   = get_trimmed_text(cards, ".styles_reviewContent__tuXiN h2"),
@@ -34,22 +33,51 @@ extract_page_data <- function(company, page_url) {
   )
 }
 
-# Company-wise scraper ---------------------------------------------------
-cmpns <- list(
-  "Ebay.com" = "https://www.trustpilot.com/review/ebay.com?page=",
-  "Amazon.com" = "https://www.trustpilot.com/review/amazon.com?page="
-)
 
-get_company_reviews <- function(company, num_pages) {
-  message(company)
-  map_dfr(1:num_pages, function(i) {
-    message(sprintf("Page %s", i))
-    extract_page_data(company, paste0(cmpns[[company]], i))
-  })
+
+
+all_reviews <- list()
+i <- 25
+repeat {
+  page_url <- sprintf("https://www.trustpilot.com/review/www.shein.com?page=%s", i)
+  if (i > 25) break;
+  message(sprintf("Scraping page %s", i))
+  
+  rvs <- extract_page_data("Shein.com", paste0(page_url, i))
+  
+  
+  all_reviews[[i]] <- rvs
+  i <- i + 1
+  Sys.sleep(sample(3:8, 1))
 }
+reviews_df <- bind_rows(all_reviews)
+
+low_rated <- reviews_df %>% filter(rating <= 2)
+
+library(tidytext)
+data("stop_words")
+complaints <- low_rated %>% 
+  unnest_tokens(word, review) %>%
+  filter(!word %in% stop_words$word, str_detect(word, "^[a-z]+$")) %>%
+  count(word, sort = T)
+
+
+top_complaints <- complaints %>%
+  slice_max(n, n = 15)
+
+
+ggplot(top_complaints, aes(fct_reorder(word, n), y = n)) +
+  geom_col(fill = "firebrick") +
+  coord_flip() +
+  labs(title = "Most Common Words in 1-2 Star Reviews", x = "Word", y = "Frequency") +
+  theme_minimal()
+
+
+
+
 
 # Main execution ---------------------------------------------------------
-df <- map_dfr(names(cmpns), get_company_reviews, num_pages = 20)
+df <- map_dfr(names(cmpns), get_company_reviews, num_pages = 100)
 
 
 # Visualization ----------------------------------------------------------
@@ -59,3 +87,7 @@ df %>%
   ggplot(aes(x = date, y = rating, col = company)) +
   geom_line() +
   geom_point(aes(size = N))
+
+
+
+
