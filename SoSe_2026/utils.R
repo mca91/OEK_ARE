03bd3f3ecade14246f1a07e8011c6e3cede8d43b
Schmelzer Martin <martin.schmelzer@gelsenkirchen.de>
2026-03-03 11:38:45 +0100

    Adding utils.R

---
+++ SoSe_2026/utils.R
@@ -0,0 +1,18 @@
+# ICONS
+ICONS <- list()
+ICONS$desktop <- fontawesome::fa("desktop", fill = "#004c93")
+ICONS$checker_green <- fontawesome::fa("check", fill = "green")
+ICONS$times_red <- fontawesome::fa("times", fill = "red")
+
+# PROGRESSBAR
+xaringanExtra::use_progress_bar(color = "#004c93", location = "bottom")
+
+# COPY TO CLIPBOARD
+htmltools::tagList(
+  xaringanExtra::use_clipboard(
+    button_text = "<i class=\"fa fa-clipboard\"></i>",
+    success_text = "<i class=\"fa fa-check\" style=\"color: #00ff00\"></i>",
+    error_text = "<i class=\"fa fa-times-circle\" style=\"color: #F94144\"></i>"
+  ),
+  rmarkdown::html_dependency_font_awesome()
+)
